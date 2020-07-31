9b610323c17a974db678ab608c83d3ef4b60e737
COil <vernet.loic@gmail.com>
2020-07-31 19:14:39 +0200

    ctrl: separated the answer controller

--- src/Controller/AnswerController.php
+++ src/Controller/AnswerController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AppController extends AbstractController
+class AnswerController extends AbstractController
 {
     /**
-     * @Route("/about", name="about")
+     * @Route("/", name="home")
      */
-    public function about(): Response
+    public function index(): Response
     {
-        return $this->render('app/about.html.twig');
+        return $this->render('answer/show.html.twig');
     }
 }
