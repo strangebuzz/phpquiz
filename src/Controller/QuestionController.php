bd081975b8d660e006f6ad92e07a53e13a781595
COil <vernet.loic@gmail.com>
2020-08-26 17:56:38 +0200

    added the random action + cleanup

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -9,6 +9,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
+ * @Route("/question", name="question_")
+ *
  * @see QuestionControllerTest
  */
 class QuestionController extends AbstractController
@@ -23,15 +25,29 @@ class QuestionController extends AbstractController
     }
 
     /**
-     * @Route("/question/{id}", name="show", requirements={"id"="\d+"})
-     * @Route("/question/{id}.json", name="show_json", defaults={"_format": "json"})
+     * @Route("/{id}", name="show", requirements={"id"="\d+"})
+     * @Route("/{id}.json", name="show_json", defaults={"_format": "json"})
      */
     public function show(int $id, string $_route): Response
     {
         $question = $this->questionData->getQuestion($id);
-        if ($_route === 'show_json') {
+        if ($_route === 'question_show_json') {
             return $this->json($question, Response::HTTP_OK, [], ['groups' => 'show']);
         }
+
+        return $this->render('question/show.html.twig', [
+            'question' => $question,
+            'code' => $this->sourceExtension->getSource($question),
+            'count' => $this->questionData->count()
+        ]);
+    }
+
+    /**
+     * @Route("/random", name="random")
+     */
+    public function random(): Response
+    {
+        $question = $this->questionData->getRandomQuestion();
 
         return $this->render('question/show.html.twig', [
             'question' => $question,
--- src/Data/QuestionData.php
+++ src/Data/QuestionData.php
@@ -4,15 +4,18 @@ namespace App\Data;
 
 use App\Entity\Question;
 use App\Repository\QuestionRepository;
+use Doctrine\DBAL\Connection;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class QuestionData
 {
     private QuestionRepository $questionRepository;
+    private Connection $connection;
 
-    public function __construct(QuestionRepository $questionRepository)
+    public function __construct(QuestionRepository $questionRepository, Connection $connection)
     {
         $this->questionRepository = $questionRepository;
+        $this->connection = $connection;
     }
 
     public function getQuestion(int $id): Question
@@ -28,5 +31,20 @@ class QuestionData
     public function count(): int
     {
         return $this->questionRepository->count([]);
+    }
+
+    public function getRandomId(): int
+    {
+        $sql = 'SELECT id FROM question ORDER BY RANDOM() LIMIT 1';
+        $id = $this->connection->fetchAll($sql)[0]['id'] ?? null;
+
+        return (int) $id;
+    }
+
+    public function getRandomQuestion(): Question
+    {
+        $id = $this->getRandomId();
+
+        return $this->getQuestion($id);
     }
 }
