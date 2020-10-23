adefc59207de80ec6699e0ce859be8f87b29c4b9
COil <vernet.loic@gmail.com>
2020-10-23 23:03:31 +0200

    tests: better tests

---
+++ tests/ApiPlatform/QuestionsTest.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests\ApiPlatform;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\Question;
+
+class QuestionsTest extends ApiTestCase
+{
+    /**
+     * Route: api_questions_get_item
+     */
+    public function testGet(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/questions/1');
+        self::assertResponseIsSuccessful();
+        self::assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        self::assertJson($client->getResponse()->getContent());
+        self::assertMatchesResourceItemJsonSchema(Question::class);
+        $json = <<<EOT
+{ 
+  "@context": "/api/contexts/Question",
+  "@id": "/api/questions/1",
+  "@type": "Question",
+  "id": 1,
+  "correctAnswerCode": "A"
+}
+EOT;
+        self::assertJsonStringEqualsJsonString($json, $client->getResponse()->getContent());
+    }
+}
